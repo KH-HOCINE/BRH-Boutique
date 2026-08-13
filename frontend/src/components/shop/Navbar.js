63b14429ba79b94db57f5fc965c78a11fae44865
Hocine KH <khendekhocine7@gmail.com>
2026-08-13 16:22:26 +0100

    panier logo

--- frontend/src/components/shop/Navbar.js
+++ frontend/src/components/shop/Navbar.js
@@ -140,10 +140,9 @@ export default function Navbar() {
 
         {/* Panier */}
         <Link to="/panier" className="navbar-cart" onClick={closeMenu}>
-          <svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8">
-            <path d="M6 2L3 6v14a2 2 0 002 2h14a2 2 0 002-2V6l-3-4z" />
-            <line x1="3" y1="6" x2="21" y2="6" />
-            <path d="M16 10a4 4 0 01-8 0" />
+          <svg width="26" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8">
+            <path d="M5 8h18l-1.5 13a1 1 0 01-1 1H7.5a1 1 0 01-1-1L5 8z" />
+            <path d="M10 8V6a4 4 0 018 0v2" />
           </svg>
           {itemCount > 0 && <span className="cart-badge">{itemCount}</span>}
         </Link>
